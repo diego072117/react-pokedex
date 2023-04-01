2577a067a65c158d6d54dc44d3d3982a25e566f6
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-03-31 19:59:19 -0500

    feat(pokeDetail): details pokemon.

--- src/pages/PokeDetail/index.tsx
+++ src/pages/PokeDetail/index.tsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom";
+import { PokemonDetail } from "../../components/PokemonDetail";
+import { usePokemon } from "../../hooks/usePokemon";
+
 export const PokeDetail = () => {
-    return (
-        <div>Details</div>
-    )
-}
+  const { pokeId } = useParams();
+  const { pokemon } = usePokemon("", pokeId);
+
+  return <PokemonDetail pokemon={pokemon!} />;
+};
