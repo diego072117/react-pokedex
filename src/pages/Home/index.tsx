12e38276ee3c0547ae8fe4a1958f1c930b3ca23a
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-03-31 19:20:20 -0500

    feat(pagination): pagination.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,34 @@
 import { useContext } from "react";
 import { PokeballIconSmall } from "../../assets/pokeball"
+import { Pagination } from "../../components/Paginarion";
 import { PokemonList } from "../../components/PokemonList"
 import { PokemonContext } from "../../context/PokemonContext";
+import { usePagination } from "../../hooks/usePagination";
 import styles from "./style.module.scss"
 
 export const Home = () => {
 
     const { pokemonsFiltered } = useContext(PokemonContext);
+    const { page, nextPage, previousPage, backToHome } = usePagination();
+
+    let perPage = 12
 
     return (
         <div className={styles.home}>
             <header>
                 <div>
-                    <PokeballIconSmall/>
+                    <PokeballIconSmall />
                     <span>Pokedex</span>
                 </div>
             </header>
-            <PokemonList pokemonsUrls={pokemonsFiltered}/>
+            <PokemonList page={page} perPage={perPage} pokemonsUrls={pokemonsFiltered} />
+            <Pagination
+                page={page}
+                perPage={perPage}
+                nextPage={nextPage}
+                previousPage={previousPage}
+                maxItems={pokemonsFiltered?.length!}
+            />
         </div>
     )
 }
